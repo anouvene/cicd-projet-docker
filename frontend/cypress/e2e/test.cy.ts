c78c964fe509d5e7e1fa98a925b7dc203cbc4ec3
Tuan <nouvenbe.antoine@gmail.com>
2025-04-12 18:41:21 +0200

    Test e2e

--- frontend/cypress/e2e/test.cy.ts
+++ frontend/cypress/e2e/test.cy.ts
@@ -3,34 +3,35 @@ describe('Vue 3 App', () => {
     cy.visit('/');
   });
 
-  it('Visite la page d\'accueil', () => {
-    cy.visit('/');
-    cy.get('body').screenshot();
-    cy.get('body').invoke('html').then(html => {
-      console.log('DOM:', html);
-    });
+  it("Visite la page d'accueil", () => {
+    cy.contains('h1', 'Home');
   });
 
-  // it('Visite la page de register', () => {
-  //   cy.contains('Inscription').click();
-  //   cy.contains('h2', 'Inscription');
-  // });
+  it('Visite la page de login', () => {
+    cy.contains('Connexion').click();
+    cy.contains('h2', 'Connexion');
+  });
 
-  // it("L'inscription fonctionne", () => {
-  //   cy.contains('Inscription').click();
-  //   cy.get('#name').type('test');
-  //   cy.get('#email').type('test@test.fr');
-  //   cy.get('#password').type('riuehuhrfezhop');
-  //   cy.get('button').contains('Inscription').click();
-  //   cy.contains('h2', 'Connexion');
-  // });
+  it('Visite la page de register', () => {
+    cy.contains('Inscription').click();
+    cy.contains('h2', 'Inscription');
+  });
 
-  // it('La connexion fonctionne', () => {
-  //   cy.contains('Connexion').click();
-  //   cy.get('#email').type('test@test.fr');
-  //   cy.get('#password').type('riuehuhrfezhop');
-  //   cy.get('button').contains('Connexion').click();
-  //   cy.contains('Mon profil');
-  //   cy.contains('Déconnexion');
-  // });
+  it("L'inscription fonctionne", () => {
+    cy.contains('Inscription').click();
+    cy.get('#name').type('test');
+    cy.get('#email').type('test@test.fr');
+    cy.get('#password').type('riuehuhrfezhop');
+    cy.get('button').contains('Inscription').click();
+    cy.contains('h2', 'Connexion');
+  });
+
+  it('La connexion fonctionne', () => {
+    cy.contains('Connexion').click();
+    cy.get('#email').type('test@test.fr');
+    cy.get('#password').type('riuehuhrfezhop');
+    cy.get('button').contains('Connexion').click();
+    cy.contains('Mon profil');
+    cy.contains('Déconnexion');
+  });
 });
