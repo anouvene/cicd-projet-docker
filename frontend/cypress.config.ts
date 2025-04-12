5ceca506d347ea2869fa07e96bb1809b5e1977f3
Tuan <nouvenbe.antoine@gmail.com>
2025-04-12 05:18:27 +0200

    fix e2e test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:4173',
     setupNodeEvents(on: any, config: any) {
       require('@cypress/code-coverage/task')(on, config);
       return config;
