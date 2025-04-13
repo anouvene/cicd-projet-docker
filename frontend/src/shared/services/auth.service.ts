cc4288fb3a7a85c5e2ce1bc66254a3d76c764784
Tuan <nouvenbe.antoine@gmail.com>
2025-04-13 03:03:20 +0200

    Update auth  service frontend

--- frontend/src/shared/services/auth.service.ts
+++ frontend/src/shared/services/auth.service.ts
@@ -1,6 +1,9 @@
 import type { LoginForm, User } from '../interfaces/index';
 
-const BASE_URL = '/api/auth';
+//const BASE_URL = '/api/auth';
+
+const BASE_PATH = import.meta.env.BASE_URL || '/';
+const BASE_URL = `${BASE_PATH}api/auth`;
 
 export async function login(loginForm: LoginForm): Promise<User> {
   const response = await fetch(BASE_URL, {
